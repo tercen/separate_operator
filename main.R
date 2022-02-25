cbcd2fe3f2d1e3818f973492c5124534722c61eb
lucaslelann <lucas.lelann@gmail.com>
2022-02-25 14:12:06 +0000

    ci(tests): add github workflows and setup tests folder

--- main.R
+++ main.R
@@ -1,22 +1,20 @@
 library(tercen)
 library(dplyr)
 library(tidyr)
- 
-ctx = tercenCtx()
- 
+
+ctx <- tercenCtx()
+
 if (length(ctx$rnames) != 1) stop("One and only one row factor is required.") 
- 
-into = strsplit(ctx$op.value('into'), ',')[[1]]
-sep = ctx$op.value('sep')
-   
+
+into <- strsplit(ctx$op.value('into'), ',')[[1]]
+sep <- ctx$op.value('sep')
+
 ctx %>% 
   rselect() %>%
   separate(1, into, sep = sep, convert = TRUE, extra='drop', fill='right') %>%
   mutate_if(is.integer, as.double) %>%
   mutate_if(is.double, function(x){ x[is.na(x)] = NaN ; x }) %>%
   mutate_if(is.character, function(x){ x[is.na(x)] = 'NA' ; x }) %>%
-  mutate(.ri = seq_len(nrow(.))-1) %>%
+  mutate(.ri = seq_len(nrow(.))-1L) %>%
   ctx$addNamespace() %>%
   ctx$save()
-  
- 
